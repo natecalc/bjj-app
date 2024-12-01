8fc2221686f47a3f1766dbe46ab089504a482947
Nate <nate@calculated.fi>
2024-12-02 09:35:02 +1100

    setting up pg admin and backend wip

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,14 @@
 import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 import { note } from "./note";
+import { config } from "dotenv";
+
+config();
 
 const app = new Elysia();
 
 app.use(swagger()).get("/ping", "pong").use(note).listen(3000);
+
+console.log(`Server is running on port ${process.env.PORT || 3000}`);
+
+export type App = typeof app;
