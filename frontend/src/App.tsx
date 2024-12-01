c602ba027cf8bda9711a395ac69cce0d148e35c9
Nate <nate@calculated.fi>
2024-12-01 15:26:45 +1100

    Initial Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import "./App.css";
+import { createRoot } from "react-dom/client";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+function App() {
+  return (
+    <main>
+      <div className="w-full bg-red-400 items-center text-center p-12">GM</div>
+    </main>
+  );
+}
+
+export default App;
