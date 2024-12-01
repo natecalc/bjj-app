af81cb441ef98352df13843ad32190c778bd9ea1
Nate <nate@calculated.fi>
2024-12-01 22:30:06 +1100

    elysia setup and learning

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import { note } from "./note";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+app.use(swagger()).get("/ping", "pong").use(note).listen(3000);
